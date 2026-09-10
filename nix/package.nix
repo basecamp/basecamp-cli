902b2c413f2973f148ef05a7628cc432babb9cb0
Jeremy Daer <jeremy@37signals.com>
2026-09-09 22:45:55 -0700

    Pin basecamp-sdk to v0.18.0

--- nix/package.nix
+++ nix/package.nix
@@ -8,7 +8,7 @@ buildGoModule.override { go = go_1_26; } (finalAttrs: {
   src = lib.cleanSource ./..;
 
   # To update: set to lib.fakeHash, run `nix build`, use the hash from the error.
-  vendorHash = "sha256-nWEquNPUTr46bza902GOP41Y3RLjDG+HcNGdskQYx4A=";
+  vendorHash = "sha256-MdVsRrJ3SEEtFFU6X2gP5s414kEhulSkxLqoD9GxiIg=";
 
   subPackages = [ "cmd/basecamp" ];
 
